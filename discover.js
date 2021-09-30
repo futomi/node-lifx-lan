e9d5303fef2fa51a2f0bffa0832aedf0708a9b2b
Joseph Ottinger <joeo@enigmastation.com>
2021-09-30 09:49:25 -0400

    Updating to include temperature information from LiFX

--- discover.js
+++ discover.js
@@ -3,5 +3,6 @@ const lifx=require('./lib/lifx-lan')
 lifx.discover().then((devs)=>{
     for(const dev of devs) {
         console.log(dev.deviceInfo)
+        //dev.setColor({color:{hue:0, saturation:1}})
     }
 })
